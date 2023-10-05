9e4c2cf7b11bd04945b8fd5a0028002fae7e9ffa
Jadson Costa <104212128+jadsonoc@users.noreply.github.com>
2023-10-05 11:43:34 -0300

    Update Start.java

--- src/main/java/dev/rpmhub/Start.java
+++ src/main/java/dev/rpmhub/Start.java
@@ -15,4 +15,18 @@ public class Start {
         return "Hello from RESTEasy";
     }
 
+    @POST
+    @Path("/kmh2mih/{kmh}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String kmh2mih(@PathParam("kmh") String km) {
+        return Double.valueOf(km) * 0.621371;
+    }
+
+    @GET
+    @Path("/no2kmh/{node}")
+    @Produces(MediaType.JSON)
+    public String no2kmh(@PathParam("node") String node) {
+        return Double.valueOf(node) * 1.852;
+    }
+
 }
